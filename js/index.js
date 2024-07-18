5a3b4a60a20aeb9226545f9d6601be050a494ce9
Richard-Raph <richardakpan77@gmail.com>
2024-07-18 09:37:57 +0100

    created site with html, css and little javascript

---
+++ js/index.js
@@ -0,0 +1,52 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav div a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav div a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+const menuToggle=document.querySelector('.toggle')
+const contact=document.querySelector('.contact')
+    
+menuToggle.addEventListener('click', ()=> {
+    menuToggle.classList.toggle('active')
+    contact.classList.toggle('active')
+});
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.homie, .heading', { origin: 'top'});
+ScrollReveal().reveal('.services-container, .project-box, .contact .contacts', { origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
